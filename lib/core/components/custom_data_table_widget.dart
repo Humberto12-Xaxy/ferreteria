91f11a5de363274e57296833969842f9c7e812af
Javier Gtz <javier@1erotrader.com>
2025-02-28 20:13:51 -0600

    adding actions to the table

--- lib/core/components/custom_data_table_widget.dart
+++ lib/core/components/custom_data_table_widget.dart
@@ -12,9 +12,10 @@ class CustomDataTable extends StatefulWidget {
   final TextAlign? headerTextAlign;
   final Color? activeColor;
   final EdgeInsets padding;
-  final BoxBorder? borderTable;
+  // final BoxBorder? borderTable;
   final TypeSelection typeSelection;
   final Function(RowElement rowSelected)? onRowSelected;
+  final double? heightRow;
 
   const CustomDataTable({
     super.key,
@@ -26,11 +27,12 @@ class CustomDataTable extends StatefulWidget {
     this.headerTextStyle = const TextStyle(fontWeight: FontWeight.bold),
     this.activeColor,
     this.padding = const EdgeInsets.all(10),
-    this.borderTable,
+    // this.borderTable,
     this.textAlign,
     this.headerTextAlign,
-    required this.typeSelection,
+    this.typeSelection = TypeSelection.single,
     this.onRowSelected,
+    this.heightRow = 60,
   });
 
   @override
@@ -40,24 +42,10 @@ class CustomDataTable extends StatefulWidget {
 class _CustomDataTableState extends State<CustomDataTable> {
   final FocusNode _focusNode = FocusNode();
   List<RowElement> selectedRows = [];
-
   @override
   void initState() {
     super.initState();
-    if (widget.headers.isEmpty) {
-      throw Exception('Headers cannot be empty');
-    }
-    if (widget.headers.length != widget.rows.first.cells.length) {
-      throw Exception('Headers and rows must have the same length');
-    }
-
-    // _focusNode.addListener(() {
-    //   if (_focusNode.hasFocus) {
-    //     HardwareKeyboard.instance.addHandler(_handleKeyEvent);
-    //   } else {
-    //     HardwareKeyboard.instance.removeHandler(_handleKeyEvent);
-    //   }
-    // });
+    verifyErrors();
   }
 
   @override
@@ -65,27 +53,6 @@ class _CustomDataTableState extends State<CustomDataTable> {
     _focusNode.dispose();
     super.dispose();
   }
-
-  // bool _handleKeyEvent(KeyEvent event) {
-  //   // Evento que recibe las teclas presionadas
-  //   if (event is KeyDownEvent) {
-  //     final index = widget.rows.indexOf(selectedRows.first);
-  //     if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
-  //       // Mover el selectedRows hacia abajo si es posible
-  //       print('Movemos hacia abajo');
-  //       if (index < widget.rows.length - 1) {
-  //         selectRow(widget.rows[index + 1]);
-  //       }
-  //     } else if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
-  //       // Mover el selectedRows hacia arriba si es posible
-  //       print('Movemos hacia arriba');
-  //       if (index > 0) {
-  //         selectRow(widget.rows[index - 1]);
-  //       }
-  //     }
-  //   }
-  //   return false;
-  // }
 
   void selectRow(RowElement row) {
     if (selectedRows.contains(row)) {
@@ -102,79 +69,128 @@ class _CustomDataTableState extends State<CustomDataTable> {
 
   @override
   Widget build(BuildContext context) {
-    final border = widget.borderTable ??
-        Border(bottom: BorderSide(color: Colors.black, width: 1));
+    addingDataCelsIfThereIsAnActionColumn();
+    // final border = widget.borderTable ??
+    //     Border(bottom: BorderSide(color: Colors.black, width: 1));
     final textAlign = widget.textAlign ?? TextAlign.left;
     final headerTextAlign = widget.headerTextAlign ?? TextAlign.left;
     final activeColor = widget.activeColor ?? Colors.blue;
     return Padding(
       padding: widget.padding,
-      child: GestureDetector(
-        onTap: () {
-          print('Focus colocado');
-        },
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: widget.headerBackgroundColor,
-              ),
-              child: Row(
-                children: widget.headers
-                    .map((e) => Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: border,
-                            ),
-                            padding: EdgeInsets.all(8),
-                            color: e.backgroundColor,
-                            child: Text(
-                              e.text,
-                              textAlign: e.align ?? headerTextAlign,
-                              style: e.textStyle ?? widget.headerTextStyle,
-                            ),
-                          ),
-                        ))
-                    .toList(),
-              ),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: widget.headerBackgroundColor,
             ),
-            Expanded(
-                child: ListView.builder(
-              itemCount: widget.rows.length,
-              itemBuilder: (context, index) {
-                final row = widget.rows[index];
-                return InkWell(
-                  onTap: () {
-                    selectRow(row);
-                    widget.onRowSelected?.call(widget.rows[index]);
-                  },
+            child: Row(
+              children: widget.headers
+                  .map((e) => Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                                bottom:
+                                    BorderSide(color: Colors.black, width: 2)),
+                          ),
+                          padding: EdgeInsets.all(8),
+                          color: e.backgroundColor,
+                          child: Text(
+                            e.text,
+                            textAlign: e.align ?? headerTextAlign,
+                            style: e.textStyle ?? widget.headerTextStyle,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ),
+          Expanded(
+              child: ListView.separated(
+            separatorBuilder: (context, index) => Divider(),
+            itemCount: widget.rows.length,
+            itemBuilder: (context, index) {
+              final row = widget.rows[index];
+              return InkWell(
+                onTap: widget.onRowSelected == null
+                    ? null
+                    : () {
+                        selectRow(row);
+                        widget.onRowSelected?.call(widget.rows[index]);
+                      },
+                child: SizedBox(
+                  height: widget.heightRow,
                   child: Row(
                     children: row.cells
                         .map((e) => Expanded(
                               child: Container(
                                 decoration: BoxDecoration(
-                                  border: border,
+                                  // border: border,
                                   color: selectedRows.contains(row)
                                       ? activeColor
                                       : e.backgroundColor,
                                 ),
                                 padding: EdgeInsets.all(8),
-                                child: Text(
-                                  e.text,
-                                  textAlign: e.align ?? textAlign,
-                                  style: e.textStyle ?? widget.textStyle,
-                                ),
+                                child: e.content != null
+                                    ? MediaQuery.removePadding(
+                                        context: context,
+                                        child: e.content!,
+                                      )
+                                    : Text(
+                                        e.text,
+                                        textAlign: e.align ?? textAlign,
+                                        style: e.textStyle ?? widget.textStyle,
+                                      ),
                               ),
                             ))
                         .toList(),
                   ),
-                );
-              },
-            )),
-          ],
-        ),
+                ),
+              );
+            },
+          )),
+        ],
       ),
     );
+  }
+
+  void verifyErrors() {
+    print('Verificando errores');
+    // Verify if headers is empty
+    if (widget.headers.isEmpty) {
+      throw Exception('Headers cannot be empty');
+    }
+
+    final amountHeaders =
+        widget.headers.where((header) => !header.isActionColumn).length;
+    final amountRows = widget.rows.first.cells.length;
+    // Verify if all rows have the same length
+    widget.rows.any(
+      (element) {
+        if (element.cells.length != amountRows) {
+          throw Exception(
+              'The row $element hanst the same length of others rows');
+        }
+        return false;
+      },
+    );
+
+    // Verify if headers and rows have the same length
+    if (amountHeaders != amountRows) {
+      throw Exception('Headers and rows must have the same length');
+    }
+  }
+
+  void addingDataCelsIfThereIsAnActionColumn() {
+    for (var header in widget.headers) {
+      print('HOla');
+      if (header.isActionColumn) {
+        print('Se añade una columna de acción');
+        for (var row in widget.rows) {
+          row.cells.add(DataCellElement(
+              text: '', content: header.action!.copyWith(rowAssigned: row)));
+        }
+      }
+    }
   }
 }
 
@@ -183,9 +199,15 @@ class HeaderElement {
   final TextAlign? align;
   final Color? backgroundColor;
   final TextStyle? textStyle;
-
+  final bool isActionColumn;
+  final ActionWidget? action;
   HeaderElement(
-      {this.backgroundColor, this.textStyle, required this.text, this.align});
+      {this.backgroundColor,
+      this.action,
+      this.textStyle,
+      required this.text,
+      this.align,
+      this.isActionColumn = false});
 }
 
 class RowElement {
@@ -194,22 +216,175 @@ class RowElement {
   RowElement({required this.cells});
 
   @override
-  String toString() => 'RowElement(cells: $cells)';
+  String toString() => cells.toString();
 }
 
 class DataCellElement {
   final String text;
+  final Widget? content;
   final TextAlign? align;
   final Color? backgroundColor;
   final TextStyle? textStyle;
 
   DataCellElement(
-      {this.backgroundColor, this.textStyle, required this.text, this.align});
+      {this.content,
+      this.backgroundColor,
+      this.textStyle,
+      required this.text,
+      this.align});
 
   @override
   String toString() {
-    return 'DataCellElement(text: $text, align: $align, backgroundColor: $backgroundColor, textStyle: $textStyle)';
+    return text;
   }
 }
 
 enum TypeSelection { single, multiple }
+
+// Tendremos que CREAR una clase que se llamara AccionWidget que extendera de Widget
+// debido a que contruiremos widgets custom para la columna de acciones
+
+/// An abstract class that represents a widget with an associated row element.
+///
+/// This class extends [StatelessWidget] and provides a way to associate a
+/// [RowElement] with the widget. It also includes a method to create a copy
+/// of the widget with a potentially different [RowElement].
+abstract class ActionWidget extends StatelessWidget {
+  /// The [RowElement] associated with this widget.
+  final RowElement? _rowAssigned;
+
+  /// Creates an [ActionWidget] with an optional [RowElement].
+  ///
+  /// The [rowAssigned] parameter can be used to associate a [RowElement]
+  /// with this widget.
+  const ActionWidget({super.key, RowElement? rowAssigned})
+      : _rowAssigned = rowAssigned;
+
+  /// Returns the [RowElement] associated with this widget.
+  RowElement? get rowAssigned => _rowAssigned;
+
+  /// Creates a copy of this [ActionWidget] with an optional new [RowElement].
+  ///
+  /// The [rowAssigned] parameter can be used to specify a new [RowElement]
+  /// for the copied widget. If not provided, the current [rowAssigned] will
+  /// be used.
+  ActionWidget copyWith({RowElement? rowAssigned});
+}
+
+// Ahora con la clase anterior podremos crear widgets custo m para la columna de acciones
+// por ejemplo un boton para las acciones
+
+/// A widget that represents a button action within a data table.
+///
+/// The [ActionButton] widget is used to create an icon button that performs
+/// an action when pressed. It extends the [ActionWidget] class.
+///
+/// The [onRowSelected] callback is triggered when the button is pressed,
+/// passing the selected row as an argument.
+///
+/// The [icon] parameter specifies the icon to be displayed on the button.
+///
+/// Example usage:
+/// ```dart
+/// ActionButton(
+///   onRowSelected: (row) {
+///     // Handle row selection
+///   },
+///   icon: Icons.edit,
+/// )
+/// ```
+///
+/// See also:
+/// - [ActionWidget], the base class for action widgets.
+class ActionButton extends ActionWidget {
+  final Function(RowElement rowSelected) onRowSelected;
+  final IconData icon;
+  final Color? colorButton;
+  final Color? colorIcon;
+
+  const ActionButton({
+    required this.onRowSelected,
+    required this.icon,
+    this.colorButton,
+    this.colorIcon,
+    super.key,
+    super.rowAssigned,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onRowSelected(rowAssigned!);
+      },
+      child: Container(
+        height: 30,
+        width: 30,
+        decoration: BoxDecoration(
+            color: colorButton ?? Theme.of(context).primaryColor,
+            shape: BoxShape.circle),
+        child: Icon(
+          icon,
+          size: 20,
+          color: colorIcon ?? Colors.white,
+        ),
+      ),
+    );
+    // return Align(
+    //   alignment: Alignment.center,
+    //   child: IconButton(
+    //     padding: EdgeInsets.zero, // Elimina padding predeterminado
+    //     constraints: const BoxConstraints(), // Evita restricciones internas
+    //     icon: Icon(
+    //       icon,
+    //       size: 20,
+    //     ),
+    //     onPressed: () {
+    //       onRowSelected(rowAssigned!);
+    //     },
+    //   ),
+    // );
+  }
+
+  @override
+  ActionButton copyWith({RowElement? rowAssigned}) {
+    return ActionButton(
+      colorButton: colorButton,
+      colorIcon: colorIcon,
+      onRowSelected: onRowSelected,
+      icon: icon,
+      rowAssigned: rowAssigned ?? this.rowAssigned,
+    );
+  }
+}
+
+class RowActionButtons extends ActionWidget {
+  final List<ActionButton> actions;
+  final double spacing;
+  const RowActionButtons(
+      {super.key,
+      required this.actions,
+      required this.spacing,
+      super.rowAssigned});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: spacing,
+      children: actions,
+    );
+  }
+
+  @override
+  ActionWidget copyWith({RowElement? rowAssigned}) {
+    final actionsWithRowAssigned = actions
+        .map((action) => action.copyWith(rowAssigned: rowAssigned))
+        .toList();
+
+    return RowActionButtons(
+        actions: actionsWithRowAssigned,
+        spacing: spacing,
+        rowAssigned: rowAssigned ?? this.rowAssigned);
+  }
+}
--- lib/features/home/presentation/screens/home/widgets/search_producs_widget.dart
+++ lib/features/home/presentation/screens/home/widgets/search_producs_widget.dart
@@ -5,19 +5,46 @@ import '../../../../../../core/components/custom_data_table_widget.dart';
 class SearchProducsWidget extends StatelessWidget {
   SearchProducsWidget({super.key});
 
-  final List<HeaderElement> columns = [
-    HeaderElement(text: 'Nombre'),
+  final List<HeaderElement> headers = [
+    HeaderElement(
+      text: 'Nombre',
+    ),
     HeaderElement(text: 'Precio'),
     HeaderElement(text: 'Cantidad'),
+    HeaderElement(
+        text: 'Acciones',
+        align: TextAlign.center,
+        isActionColumn: true,
+        action: RowActionButtons(actions: [
+          ActionButton(
+              onRowSelected: (rowSelected) {
+                print('EDITAR: $rowSelected');
+              },
+              icon: Icons.edit),
+          ActionButton(
+              onRowSelected: (rowSelected) {
+                print('ELIMINAR: $rowSelected');
+              },
+              icon: Icons.delete),
+        ], spacing: 10)
+        // action: ButtonAction(
+        //     onRowSelected: (rowSelected) {
+        //       print('Row selected: $rowSelected');
+        //     },
+        //     icon: Icons.edit),
+        ),
   ];
 
   final List<RowElement> rows = List.generate(
     50,
     (int index) => RowElement(
       cells: [
-        DataCellElement(text: 'Nombre $index'),
+        DataCellElement(
+          text: 'Nombre $index',
+        ),
         DataCellElement(text: 'precio $index'),
         DataCellElement(text: 'cantidad $index'),
+        // DataCellElement(text: 'cantidad $index'),
       ],
     ),
   );
@@ -57,21 +84,11 @@ class SearchProducsWidget extends StatelessWidget {
           Expanded(
               flex: 1,
               child: CustomDataTable(
-                  typeSelection: TypeSelection.single,
-                  onRowSelected: (rowSelected) {
-                    print('Row selected: ${rowSelected.cells.map(
-                      (e) {
-                        return e.text;
-                      },
-                    ).toList()}');
-                  },
-                  headers: [
-                    HeaderElement(
-                      text: 'Header 1',
-                    ),
-                    HeaderElement(text: 'Header 2'),
-                    HeaderElement(text: 'Header 3')
-                  ],
+                  // typeSelection: TypeSelection.single,
+                  // onRowSelected: (rowSelected) {
+                  //   print('Row selected: $rowSelected');
+                  // },
+                  headers: headers,
                   rows: rows)),
         ],
       ),
